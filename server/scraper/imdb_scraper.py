8832537d182093af842c7b1119d5a54eb54d52e3
tadeo-jab <tadeojablonski@gmail.com>
2025-01-07 20:32:59 -0300

    Animated Filter

--- server/scraper/imdb_scraper.py
+++ server/scraper/imdb_scraper.py
@@ -145,29 +145,41 @@ def get_movies(url):
 
 def scrape_data_main(query):
     new_query = json.loads(query)
-
+    url_filters = new_query.copy()
+    
     credit_ids = []
     if (new_query.get('actor', None) != None):
         credit_ids.append(get_credits(new_query['actor']))
+        del url_filters['actor']
     
     if (new_query.get('director', None) != None):
         credit_ids.append(get_credits(new_query['director']))
+        del url_filters['director']
     
     if credit_ids:
-        new_query.update({'credits': credit_ids})
+        url_filters.update({'credits': credit_ids})
 
     sort_type = random_choice(search_data['sort_options'])
     sort_order = random_choice(search_data['order_options'])
 
-    new_query.update({
+    url_filters.update({
         'sort': sort_type,
         'order': sort_order,
         'load_limit': search_data['count'],
         'excluded': search_data['excluded_genres']
     })
 
+    if new_query.get('animated', None) != None:
+        del url_filters['animated']
+        if (new_query['animated']) and (new_query.get('genres', None) != None):
+            url_filters['genres'].append('Animation')
+        elif (new_query['animated']) and (new_query.get('genres', None) == None):
+            url_filters.update({'genres': ['Animation']})
+        elif not new_query['animated']:
+            url_filters['excluded'].append('Animation')
+
     try:
-        search_url = url_builder(new_query)
+        search_url = url_builder(url_filters)
         #movie_list = get_movies_from_page('https://caching.graphql.imdb.com/?operationName=AdvancedTitleSearch&variables=%7B%22certificateConstraint%22%3A%7B%22anyRegionCertificateRatings%22%3A%5B%7B%22rating%22%3A%22R%22%2C%22region%22%3A%22US%22%7D%5D%2C%22excludeRegionCertificateRatings%22%3A%5B%5D%7D%2C%22first%22%3A50%2C%22genreConstraint%22%3A%7B%22allGenreIds%22%3A%5B%22Crime%22%2C%22Thriller%22%5D%2C%22excludeGenreIds%22%3A%5B%22Talk-Show%22%2C%22Reality-TV%22%2C%22News%22%2C%22Game-Show%22%2C%22Documentary%22%2C%22Short%22%5D%7D%2C%22locale%22%3A%22en-US%22%2C%22originCountryConstraint%22%3A%7B%22anyPrimaryCountries%22%3A%5B%22US%22%5D%7D%2C%22releaseDateConstraint%22%3A%7B%22releaseDateRange%22%3A%7B%22end%22%3A%222024-12-31%22%2C%22start%22%3A%221980-01-01%22%7D%7D%2C%22runtimeConstraint%22%3A%7B%22runtimeRangeMinutes%22%3A%7B%22max%22%3A180%2C%22min%22%3A1%7D%7D%2C%22sortBy%22%3A%22USER_RATING%22%2C%22sortOrder%22%3A%22DESC%22%2C%22titleCreditsConstraint%22%3A%7B%22allCredits%22%3A%5B%7B%22nameId%22%3A%22nm0000233%22%7D%2C%7B%22nameId%22%3A%22nm0000168%22%7D%5D%2C%22excludeCredits%22%3A%5B%5D%7D%2C%22titleTypeConstraint%22%3A%7B%22anyTitleTypeIds%22%3A%5B%22movie%22%5D%2C%22excludeTitleTypeIds%22%3A%5B%5D%7D%2C%22userRatingsConstraint%22%3A%7B%22aggregateRatingRange%22%3A%7B%22max%22%3A9.9%2C%22min%22%3A5%7D%7D%7D&extensions=%7B%22persistedQuery%22%3A%7B%22sha256Hash%22%3A%2260a7b8470b01671336ffa535b21a0a6cdaf50267fa2ab55b3e3772578a8c1f00%22%2C%22version%22%3A1%7D%7D')
 
         #movie_list = get_movies_from_page(search_url)
